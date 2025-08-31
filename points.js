207c8d603875e99af6224e1f44d8caae3a95f6ab
Hriddha19 <s2gedits10k@gmail.com>
2025-09-01 00:03:02 +0530

    Update points.js

--- points.js
+++ points.js
@@ -10,7 +10,7 @@ const teams = [
   { name: "DesTroyer", PTS: 12 },
   { name: "Supa Strikers", PTS: 6 },
   { name: "Chuha Gang", PTS: 6 },
-  { name: "T*m Haters", PTS: 0 },
+  { name: "T*m Haters", PTS: 5 },
 ];
 
 // Function to build league table
